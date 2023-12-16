8054dcbf5dfc19cf2a556704cb32fe423fefa955
gm100861 <gm100861@gmail.com>
2023-12-16 15:05:54 +0800

    字符串

--- lesson-1/c_str.c
+++ lesson-1/c_str.c
@@ -10,5 +10,10 @@
 
 int main()
 {
+    char str[] = "abcd";
+    // \0在C语言中表示的是字符串的结束符，如果数组中没有\0，将char数组当字符串使用时会出现一些随机值。
+    char str1[] = {'a', 'b', 'c', '\0'};
+    printf("%s\n", str);    // abcd
+    printf("%s\n", str1);   //abc
     return 0;
 }
